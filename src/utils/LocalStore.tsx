d45b0a3ba5e2a7c04bbd87d06f69254bed359ea0
Grégory Paul <me@gpaul.me>
2023-10-27 19:44:03 +0200

    refactor: rename local storage constant for cities key

--- src/utils/LocalStore.tsx
+++ src/utils/LocalStore.tsx
@@ -1,23 +1,20 @@
-export const LOCALSTORAGE_KEY = 'WEATHER_CITIES'
+export const LOCALSTORAGE_CITIES_KEY = 'WEATHER_CITIES'
 
 export const LOCALSTORAGE_API_KEY = 'WEATHER_CITIES_API_KEY'
 
 export const LOCALSTORAGE_REFRESH_KEY = 'WEATHER_CITIES_REFRESH_KEY'
 
 export const getCitiesFromLocalStore: () => CityOrPosition[] = () =>
-    JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY) || '[]')
+    JSON.parse(localStorage.getItem(LOCALSTORAGE_CITIES_KEY) || '[]')
 
 export const saveCitiesInLocalStore = (newCities: CityOrPosition[]) =>
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(newCities))
+    localStorage.setItem(LOCALSTORAGE_CITIES_KEY, JSON.stringify(newCities))
 
-export const saveApiKeyInLocalStore = (apiKey: string) =>
-    localStorage.setItem(LOCALSTORAGE_API_KEY, apiKey)
+export const saveApiKeyInLocalStore = (apiKey: string) => localStorage.setItem(LOCALSTORAGE_API_KEY, apiKey)
 
-export const getApiKeyFromLocalStore = () =>
-    localStorage.getItem(LOCALSTORAGE_API_KEY)
+export const getApiKeyFromLocalStore = () => localStorage.getItem(LOCALSTORAGE_API_KEY)
 
 export const saveRefreshKeyInLocalStore = (refreshKey: number) =>
     localStorage.setItem(LOCALSTORAGE_REFRESH_KEY, refreshKey.toString())
 
-export const getRefreshKeyFromLocalStore = () =>
-    localStorage.getItem(LOCALSTORAGE_REFRESH_KEY)
+export const getRefreshKeyFromLocalStore = () => localStorage.getItem(LOCALSTORAGE_REFRESH_KEY)
