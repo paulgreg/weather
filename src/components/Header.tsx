78aa5881c88c9bf8896d980b16f34694e8c578b0
Grégory Paul <me@gpaul.me>
2024-06-23 21:20:05 +0200

    feat: add refresh all cities button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
 import './Header.css'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
-import { CloudIcon, PlusIcon, WrenchIcon } from './WeatherIcon'
+import { CloudIcon, PlusIcon, RefreshIcon, WrenchIcon } from './WeatherIcon'
+import { useCallback } from 'react'
 
 const Header = () => {
     const { t } = useTranslation()
+    const onRefreshClick = useCallback((e: React.MouseEvent) => {
+        e.stopPropagation()
+        e.preventDefault()
+        if (confirm(t('refreshAll'))) window.location.reload()
+    }, [])
 
     return (
         <header>
@@ -14,13 +20,18 @@ const Header = () => {
                 </span>
                 <h1>{t('title')}</h1>
             </Link>
-            <div>
+            <div className="headerIcons">
                 <Link to="/config" title={t('config')} className="headerIcon">
                     <WrenchIcon />
                 </Link>
                 <Link to="/add" title={t('addCity')} className="headerIcon">
                     <PlusIcon />
                 </Link>
+                <span onClick={onRefreshClick} className="headerIcon" style={{ marginTop: '2px' }}>
+                    <span className="headerRefreshIcon">
+                        <RefreshIcon />
+                    </span>
+                </span>
             </div>
         </header>
     )
