e91345cc4004d176293abb93af86accca468c50e
Grégory Paul <me@gpaul.me>
2026-05-02 16:53:03 +0200

    chore: update deps

--- src/utils/useTimeout.ts
+++ src/utils/useTimeout.ts
@@ -1,5 +1,5 @@
 const useTimeout = (delay = 250) => {
-    let timeout: NodeJS.Timeout
+    let timeout: ReturnType<typeof setTimeout>
     return (fn: () => void) => {
         clearTimeout(timeout)
         timeout = setTimeout(fn, delay)
