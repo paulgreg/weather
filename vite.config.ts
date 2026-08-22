120e30a98cc8a57b287768abcdacbdf28f69a9c0
Grégory Paul <me@gpaul.me>
2026-08-22 16:30:10 +0200

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/weather/' : './',
-    plugins: [
-        svgr(),
-        react({
-            babel: {
-                plugins: ['babel-plugin-react-compiler'],
-            },
-        }),
-    ],
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/weather/' : './',
+  plugins: [
+      svgr(),
+      react({
+          babel: {
+              plugins: ['babel-plugin-react-compiler'],
+          },
+      }),
+  ],
 })
